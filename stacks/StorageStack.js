5ea0f0394080901200176847607ee773f92254e3
Dustysea <49942226+slothrobot@users.noreply.github.com>
2022-12-31 01:02:01 +0100

    Adding the API

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,6 +1,7 @@
 import {Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
+  // Create an S3 bucket
   const bucket = new Bucket(stack, "Uploads");
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
