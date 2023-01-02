bc8dba60e84b95531b7549f1115f8e7fd9547ed0
Dustysea <49942226+slothrobot@users.noreply.github.com>
2023-01-03 00:58:29 +0100

    Setting up the react app

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+import Routes from './Routes';
+import { LinkContainer } from 'react-router-bootstrap';
+
+function App() {
+  return (
+    <div className='App container py-3'>
+      <Navbar collapseOnSelect bg='light' expand='md' className='mb-3'>
+       <LinkContainer to='/'>
+        <Navbar.Brand className='font-weight-bold text-muted'>
+          Dreamemo
+        </Navbar.Brand>
+       </LinkContainer>
+       <Navbar.Toggle />
+        {/* Navbar.Collapse component ensures that on mobile devices the two links will be collapsed */}
+       <Navbar.Collapse className='justify-content-end'>
+        <Nav activeKey={window.location.pathname}>
+          <LinkContainer to='/signup'>
+            <Nav.Link>Signup</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/login'>
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+        </Nav>
+       </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import './NotFound.css';
+
+export default function NotFound() {
+    return (
+        <div className='NotFound text-center'>
+            <h3>You landed on an empty page.</h3>
+        </div>
+    );
+}
